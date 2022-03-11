0916c2ce896ace27d810e8409d0bd7cba9b89ff3
Devin Smith <devinsmith@deephaven.io>
2022-03-11 10:20:03 -0800

    latest

--- server/jetty/src/main/dist/app.d/poll-app.groovy
+++ server/jetty/src/main/dist/app.d/poll-app.groovy
@@ -1,34 +1,48 @@
+import io.deephaven.csv.parsers.DateTimeAsLongParser
+import io.deephaven.csv.parsers.LongParser
+import io.deephaven.csv.parsers.StringParser
+import io.deephaven.server.jetty.PollServlet
+import io.deephaven.csv.CsvSpecs
+
 import static io.deephaven.api.agg.Aggregation.AggCount
 import static io.deephaven.api.agg.Aggregation.AggCountDistinct
 import static io.deephaven.api.agg.Aggregation.AggMax
 import static io.deephaven.api.agg.Aggregation.AggMin
 import static io.deephaven.api.agg.Aggregation.AggAvg
 import static io.deephaven.api.agg.Aggregation.AggMed
-
+import static io.deephaven.engine.util.TableTools.merge
 import static io.deephaven.csv.CsvTools.readCsv
-import io.deephaven.csv.CsvSpecs
+
 
 poll_csv = readCsv(System.getProperty("poll.file", "poll.csv"), CsvSpecs.builder()
         .hasHeaderRow(false)
-        .putParserForName("Timestamp", io.deephaven.csv.parsers.DateTimeAsLongParser.INSTANCE)
-        .putParserForName("Address", io.deephaven.csv.parsers.StringParser.INSTANCE)
-        .putParserForName("Id", io.deephaven.csv.parsers.StringParser.INSTANCE)
-        .putParserForName("UserAgent", io.deephaven.csv.parsers.StringParser.INSTANCE)
-        .putParserForName("BestNumber", io.deephaven.csv.parsers.LongParser.INSTANCE)
+        .putParserForName("Timestamp", DateTimeAsLongParser.INSTANCE)
+        .putParserForName("Address", StringParser.INSTANCE)
+        .putParserForName("Id", StringParser.INSTANCE)
+        .putParserForName("UserAgent", StringParser.INSTANCE)
+        .putParserForName("BestNumber", LongParser.INSTANCE)
         .headers(["Timestamp" ,"Address", "Id", "UserAgent", "BestNumber"])
         .build())
 
-poll = merge(poll_csv, io.deephaven.server.jetty.PollServlet.getTable())
-poll_count = poll.countBy("Count", "BestNumber").sortDescending("Count")
+poll = merge(poll_csv, PollServlet.getTable())
+
+poll_count = poll.countBy("Count", "BestNumber")
+        .sortDescending("Count")
+
 poll_count_top_5 = poll_count
         .head(5)
         .view("Category=`C`+BestNumber", "Count")
-poll_count_rest = poll_count
-        .slice(5, Long.MAX_VALUE>>1)
-        .view("Count")
-        .sumBy()
-        .view("Category=`Others`", "Count")
-poll_results = merge(poll_count_top_5, poll_count_rest)
+
+poll_count_top_10 = poll_count
+        .head(10)
+        .view("Category=`C`+BestNumber", "Count")
+
+hits_by_minute = poll
+        .view("TimestampBin=lowerBin(Timestamp, 60000000000)")
+        .countBy("Count", "TimestampBin")
+        .sortDescending("TimestampBin")
+hits_by_min_latest = hits_by_minute.head(1).view("Count")
+hits_by_min_max = hits_by_minute.view("Count").maxBy()
 
 global_stats = poll.aggBy([
         AggCount("Count"),
@@ -40,14 +54,3 @@ global_stats = poll.aggBy([
         AggMax("MaxNumber=BestNumber"),
         AggAvg("AvgNumber=BestNumber"),
         AggMed("MedNumber=BestNumber")])
-
-poll_by_number = poll.aggBy([AggCountDistinct("UniqueAddresses=Address")], "BestNumber").sortDescending("UniqueAddresses")
-poll_by_number_top_5 = poll_by_number
-        .head(5)
-        .view("Category=`C`+BestNumber", "UniqueAddresses")
-poll_by_number_rest = poll_by_number
-        .slice(5, Long.MAX_VALUE>>1)
-        .view("UniqueAddresses")
-        .sumBy()
-        .view("Category=`Others`", "UniqueAddresses")
-poll_by_number_results = merge(poll_by_number_top_5, poll_by_number_rest)

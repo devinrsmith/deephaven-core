45511c710fa218d1dfe60c54cd7526e74b94aba8
Devin Smith <devinsmith@deephaven.io>
2022-05-17 10:45:38 -0700

    docs

--- server/config-jetty/src/main/java/io/deephaven/ssl/config/DeephavenJettySslUtils.java
+++ server/config-jetty/src/main/java/io/deephaven/ssl/config/DeephavenJettySslUtils.java
@@ -4,6 +4,7 @@ import nl.altindag.ssl.util.JettySslUtils;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class DeephavenJettySslUtils {
+
     public static SslContextFactory.Server forServer(SSLConfig config) {
         return JettySslUtils.forServer(DeephavenSslUtils.create(config));
     }

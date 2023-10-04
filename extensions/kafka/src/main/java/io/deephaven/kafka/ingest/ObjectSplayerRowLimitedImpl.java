98dfee09d694a2c6bae6b2acdaffe3031c5601a1
Devin Smith <devinsmith@deephaven.io>
2023-10-04 15:49:14 -0700

    iterable

--- extensions/kafka/src/main/java/io/deephaven/kafka/ingest/ObjectSplayerRowLimitedImpl.java
+++ extensions/kafka/src/main/java/io/deephaven/kafka/ingest/ObjectSplayerRowLimitedImpl.java
@@ -4,10 +4,14 @@ import io.deephaven.chunk.ObjectChunk;
 import io.deephaven.chunk.ResettableObjectChunk;
 import io.deephaven.chunk.WritableChunk;
 import io.deephaven.chunk.attributes.Any;
+import io.deephaven.engine.primitive.iterator.CloseableIterator;
 import io.deephaven.qst.type.Type;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 final class ObjectSplayerRowLimitedImpl<T> implements ObjectSplayerRowLimited<T> {
     private final ObjectSplayer<T> delegate;
@@ -40,14 +44,52 @@ final class ObjectSplayerRowLimitedImpl<T> implements ObjectSplayerRowLimited<T>
 
     @Override
     public void splayAll(ObjectChunk<? extends T, ?> in, List<WritableChunk<?>> out) {
-        final int inSize = in.size();
-        // Note: doing a cast since resetFromTypedChunk does not support ? extends T.
-        final ObjectChunk<T, ?> oc = in.asObjectChunk();
-        try (final ResettableObjectChunk<T, Any> slice = ResettableObjectChunk.makeResettableChunk()) {
-            for (int i = 0; i < inSize; i += maxChunkSize) {
-                // delegate.splayAll(in.slice(i, Math.min(maxChunkSize, inSize - i)), out);
-                delegate.splayAll(slice.resetFromTypedChunk(oc, i, Math.min(maxChunkSize, inSize - i)), out);
+        for (ObjectChunk<? extends T, ?> slice : iterable(in, maxChunkSize)) {
+            delegate.splayAll(slice, out);
+        }
+    }
+
+    // todo: consider adding this functionality into Chunks themselves; potentially useful abstraction.
+
+    private static <T, ATTR extends Any> Iterable<ObjectChunk<T, ATTR>> iterable(ObjectChunk<T, ATTR> source,
+            final int maxChunkSize) {
+        // Note: we need to create the "for pool" version to guarantee we are getting a plain version; we know we don't
+        // need to close them.
+        return () -> iterator(source, ResettableObjectChunk.makeResettableChunkForPool(), maxChunkSize);
+    }
+
+    private static <T, ATTR extends Any> Iterator<ObjectChunk<T, ATTR>> iterator(ObjectChunk<T, ATTR> source,
+            ResettableObjectChunk<T, ATTR> slice, final int maxChunkSize) {
+        return new ObjectChunkSliceIterator<>(source, slice, maxChunkSize);
+    }
+
+    private static class ObjectChunkSliceIterator<T, ATTR extends Any>
+            implements CloseableIterator<ObjectChunk<T, ATTR>> {
+        private final ObjectChunk<T, ATTR> source;
+        private final ResettableObjectChunk<T, ATTR> slice;
+        private final int maxChunkSize;
+        private int ix = 0;
+
+        private ObjectChunkSliceIterator(ObjectChunk<T, ATTR> source, ResettableObjectChunk<T, ATTR> slice,
+                int maxChunkSize) {
+            this.source = Objects.requireNonNull(source);
+            this.slice = slice;
+            this.maxChunkSize = maxChunkSize;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return ix < source.size();
+        }
+
+        @Override
+        public ObjectChunk<T, ATTR> next() {
+            if (ix >= source.size() || ix < 0) {
+                throw new NoSuchElementException();
             }
+            slice.resetFromTypedChunk(source, ix, Math.min(maxChunkSize, source.size() - ix));
+            ix += maxChunkSize;
+            return slice;
         }
     }
 }

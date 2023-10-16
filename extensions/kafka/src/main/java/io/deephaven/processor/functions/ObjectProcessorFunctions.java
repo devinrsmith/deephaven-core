08f8009cd596bfddd4fa67a30b0720f94fb2a0a2
Devin Smith <devinsmith@deephaven.io>
2023-10-16 13:21:36 -0700

    Update javadoc on ObjectProcessorFunctions

--- extensions/kafka/src/main/java/io/deephaven/processor/functions/ObjectProcessorFunctions.java
+++ extensions/kafka/src/main/java/io/deephaven/processor/functions/ObjectProcessorFunctions.java
@@ -6,6 +6,7 @@ package io.deephaven.processor.functions;
 import io.deephaven.chunk.ObjectChunk;
 import io.deephaven.functions.TypedFunction;
 import io.deephaven.processor.ObjectProcessor;
+import io.deephaven.qst.type.Type;
 
 import java.util.List;
 
@@ -13,7 +14,11 @@ public final class ObjectProcessorFunctions {
 
     /**
      * Creates a function-based processor whose {@link ObjectProcessor#outputTypes()} is the
-     * {@link TypedFunction#returnType()} from each function in {@code functions}.
+     * {@link TypedFunction#returnType()} from each function in {@code functions}. The {@code functions} output will be
+     * adapted as necessary to ensure compatibility with the required {@link ObjectProcessor#chunkType(Type) chunk
+     * types}. The means that callers should <b>not</b> be pre-adapting the function types to match the expected chunk
+     * types. For example, a caller with a function of type {@link Type#instantType()} should pass that function in
+     * as-is (as opposed to trying to adapt it first into a {@link Type#longType()} function).
      *
      * <p>
      * The implementation of {@link ObjectProcessor#processAll(ObjectChunk, List)} is column-oriented with a virtual

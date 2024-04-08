6f6aaaed8cd953c6263baa51a204686d9b2d1e9c
Devin Smith <devinsmith@deephaven.io>
2024-04-08 09:38:27 -0700

    Remove json-table

--- extensions/bson-jackson/src/main/java/io/deephaven/bson/jackson/JacksonBsonProvider.java
+++ extensions/bson-jackson/src/main/java/io/deephaven/bson/jackson/JacksonBsonProvider.java
@@ -3,18 +3,12 @@
 //
 package io.deephaven.bson.jackson;
 
-import io.deephaven.engine.table.Table;
 import io.deephaven.json.JsonProcessorProvider;
-import io.deephaven.json.JsonPublishingProvider;
-import io.deephaven.json.JsonStreamPublisherOptions;
-import io.deephaven.json.JsonTableOptions;
 import io.deephaven.json.ValueOptions;
 import io.deephaven.json.jackson.JacksonProcessors;
-import io.deephaven.json.jackson.JacksonStreamPublisher;
-import io.deephaven.json.jackson.JacksonTable;
 
 // Not hooking up auto-service, is not the default
-public final class JacksonBsonProvider implements JsonProcessorProvider, JsonPublishingProvider {
+public final class JacksonBsonProvider implements JsonProcessorProvider {
 
     @Override
     public JacksonProcessors provider(ValueOptions options) {
@@ -24,15 +18,5 @@ public final class JacksonBsonProvider implements JsonProcessorProvider, JsonPub
     @Override
     public JacksonProcessors namedProvider(ValueOptions options) {
         return JacksonProcessors.of(options, JacksonBsonConfiguration.defaultFactory());
-    }
-
-    @Override
-    public JacksonStreamPublisher of(JsonStreamPublisherOptions options) {
-        return JacksonStreamPublisher.of(options, JacksonBsonConfiguration.defaultFactory());
-    }
-
-    @Override
-    public Table of(JsonTableOptions options) {
-        return JacksonTable.of(options, JacksonBsonConfiguration.defaultFactory());
     }
 }
--- extensions/json-jackson/src/main/java/io/deephaven/json/jackson/JacksonJsonProvider.java
+++ extensions/json-jackson/src/main/java/io/deephaven/json/jackson/JacksonJsonProvider.java
@@ -4,25 +4,16 @@
 package io.deephaven.json.jackson;
 
 import com.google.auto.service.AutoService;
-import io.deephaven.engine.table.Table;
 import io.deephaven.json.JsonProcessorProvider;
-import io.deephaven.json.JsonPublishingProvider;
-import io.deephaven.json.JsonStreamPublisher;
-import io.deephaven.json.JsonStreamPublisherOptions;
-import io.deephaven.json.JsonTableOptions;
 import io.deephaven.json.ValueOptions;
-import io.deephaven.processor.NamedObjectProcessor;
-import io.deephaven.processor.ObjectProcessor;
 
 /**
- * The Jackson {@link JsonPublishingProvider} and {@link JsonPublishingProvider}.
+ * The Jackson {@link JsonProcessorProvider}.
  *
  * @see JacksonProcessors
- * @see JacksonStreamPublisher
- * @see JacksonTable
  */
-@AutoService({JsonProcessorProvider.class, JsonPublishingProvider.class})
-public final class JacksonJsonProvider implements JsonProcessorProvider, JsonPublishingProvider {
+@AutoService({JsonProcessorProvider.class})
+public final class JacksonJsonProvider implements JsonProcessorProvider {
 
     @Override
     public JacksonProcessors provider(ValueOptions options) {
@@ -32,15 +23,5 @@ public final class JacksonJsonProvider implements JsonProcessorProvider, JsonPub
     @Override
     public JacksonProcessors namedProvider(ValueOptions options) {
         return JacksonProcessors.of(options, JacksonConfiguration.defaultFactory());
-    }
-
-    @Override
-    public JacksonStreamPublisher of(JsonStreamPublisherOptions options) {
-        return JacksonStreamPublisher.of(options, JacksonConfiguration.defaultFactory());
-    }
-
-    @Override
-    public Table of(JsonTableOptions options) {
-        return JacksonTable.of(options, JacksonConfiguration.defaultFactory());
     }
 }

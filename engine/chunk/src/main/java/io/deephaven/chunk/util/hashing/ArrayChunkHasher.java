d4e532993de1c417af1e83b5c845bf49c212b300
Devin Smith <devinsmith@deephaven.io>
2024-07-03 07:04:02 -0700

    f

---
+++ engine/chunk/src/main/java/io/deephaven/chunk/util/hashing/ArrayChunkHasher.java
@@ -0,0 +1,30 @@
+package io.deephaven.chunk.util.hashing;
+
+import java.util.Arrays;
+import java.util.function.ToIntFunction;
+
+public final class ArrayChunkHasher {
+    private static final ToIntFunction<boolean[]> BOOLEAN_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<char[]> CHAR_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<byte[]> BYTE_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<short[]> SHORT_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<int[]> INT_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<long[]> LONG_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<float[]> FLOAT_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<double[]> DOUBLE_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<Object[]> OBJECT_ARRAY_HASH = Arrays::hashCode;
+    private static final ToIntFunction<Object[]> OBJECT_ARRAY_DEEP_HASH = Arrays::deepHashCode;
+
+    private static final ChunkHasher BOOLEAN_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher CHAR_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher BYTE_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher SHORT_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher INT_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher LONG_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher FLOAT_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher DOUBLE_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher OBJECT_ARRAY_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+    private static final ChunkHasher OBJECT_ARRAY_DEEP_HASHER = new ObjectChunkFunctionHasher<>(BOOLEAN_ARRAY_HASH);
+
+
+}
---
+++ engine/chunk/src/main/java/io/deephaven/chunk/util/hashing/ObjectChunkFunctionHasher.java
@@ -0,0 +1,67 @@
+//
+// Copyright (c) 2016-2024 Deephaven Data Labs and Patent Pending
+//
+package io.deephaven.chunk.util.hashing;
+
+import io.deephaven.chunk.Chunk;
+import io.deephaven.chunk.ObjectChunk;
+import io.deephaven.chunk.WritableIntChunk;
+import io.deephaven.chunk.attributes.HashCodes;
+import io.deephaven.chunk.attributes.Values;
+
+import java.util.Objects;
+import java.util.function.ToIntFunction;
+
+import static io.deephaven.chunk.util.hashing.ChunkHasher.scrambleHash;
+
+public final class ObjectChunkFunctionHasher<T> implements ChunkHasher {
+    private final ToIntFunction<T> hashFunction;
+
+    public ObjectChunkFunctionHasher(ToIntFunction<T> hashFunction) {
+        this.hashFunction = Objects.requireNonNull(hashFunction);
+    }
+
+    @Override
+    public int hashInitial(Object value) {
+        //noinspection unchecked
+        return hashInitialSingle(hashFunction, (T) value);
+    }
+
+    @Override
+    public int hashUpdate(int existing, Object value) {
+        //noinspection unchecked
+        return hashUpdateSingle(hashFunction, existing, (T) value);
+    }
+
+    @Override
+    public void hashInitial(Chunk<Values> values, WritableIntChunk<HashCodes> destination) {
+        hashInitial(hashFunction, values.asObjectChunk(), destination);
+    }
+
+    @Override
+    public void hashUpdate(Chunk<Values> values, WritableIntChunk<HashCodes> destination) {
+        hashSecondary(hashFunction, values.asObjectChunk(), destination);
+    }
+
+    private static <T> void hashInitial(ToIntFunction<T> hashFunction, ObjectChunk<T, Values> values, WritableIntChunk<HashCodes> destination) {
+        for (int ii = 0; ii < values.size(); ++ii) {
+            destination.set(ii, hashInitialSingle(hashFunction, values.get(ii)));
+        }
+        destination.setSize(values.size());
+    }
+
+    private static <T> void hashSecondary(ToIntFunction<T> hashFunction, ObjectChunk<T, Values> values, WritableIntChunk<HashCodes> destination) {
+        for (int ii = 0; ii < values.size(); ++ii) {
+            destination.set(ii, hashUpdateSingle(hashFunction, destination.get(ii), values.get(ii)));
+        }
+        destination.setSize(values.size());
+    }
+
+    private static <T> int hashInitialSingle(ToIntFunction<T> hashFunction, T value) {
+        return scrambleHash(hashFunction.applyAsInt(value));
+    }
+
+    private static <T> int hashUpdateSingle(ToIntFunction<T> hashFunction, int existing, T newValue) {
+        return existing * 31 + hashInitialSingle(hashFunction, newValue);
+    }
+}

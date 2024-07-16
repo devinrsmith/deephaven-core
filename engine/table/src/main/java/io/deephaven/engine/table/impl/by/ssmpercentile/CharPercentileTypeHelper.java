0f1f4936e44efe1c6432dacacee0871c20d383e3
Devin Smith <devinsmith@deephaven.io>
2024-07-16 13:57:02 -0700

    Update CharPercentileTypeHelper etc

--- engine/table/src/main/java/io/deephaven/engine/table/impl/by/ssmpercentile/CharPercentileTypeHelper.java
+++ engine/table/src/main/java/io/deephaven/engine/table/impl/by/ssmpercentile/CharPercentileTypeHelper.java
@@ -103,7 +103,7 @@ public class CharPercentileTypeHelper implements SsmChunkedPercentileOperator.Pe
         while (lo < hi) {
             final int mid = (lo + hi) >>> 1;
             final char testValue = valuesToSearch.get(mid);
-            final boolean moveHi = gt(testValue, searchValue);
+            final boolean moveHi = CharComparisons.gt(testValue, searchValue);
             if (moveHi) {
                 hi = mid;
             } else {
@@ -114,11 +114,4 @@ public class CharPercentileTypeHelper implements SsmChunkedPercentileOperator.Pe
         return hi;
     }
 
-    private static int doComparison(char lhs, char rhs) {
-        return CharComparisons.compare(lhs, rhs);
-    }
-
-    private static boolean gt(char lhs, char rhs) {
-        return doComparison(lhs, rhs) > 0;
-    }
 }
